5657a3b049370d1eb9151f58f22b184f2a0834ae
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-13 09:07:48 -0300

    feat: CRUD project and tasks finish

--- app/Controllers/Http/ProjectsController.ts
+++ app/Controllers/Http/ProjectsController.ts
@@ -1,24 +1,42 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Project from 'App/Models/Project'
 
 export default class ProjectsController {
   public async index ({}: HttpContextContract) {
+    const projects = await Project.query().preload('user')
+
+    return projects
   }
 
-  public async create ({}: HttpContextContract) {
+  public async store ({ request, auth }: HttpContextContract) {
+    const data = request.only(['title', 'description'])
+    const project = await Project.create({  ...data, user_id: auth.user?.id })
+
+    return project
   }
 
-  public async store ({}: HttpContextContract) {
+  public async show ({ params }: HttpContextContract) {
+    const project = await Project.findByOrFail('id', params.id)
+
+    await project.load('user')
+    await project.load('tasks')
+
+    return project
   }
 
-  public async show ({}: HttpContextContract) {
+  public async update ({ request, params }: HttpContextContract) {
+    const project = await Project.findByOrFail('id', params.id)
+    const data = request.only(['title', 'description'])
+
+    project.merge(data)
+
+    await project.save()
+
+    return project
   }
 
-  public async edit ({}: HttpContextContract) {
-  }
-
-  public async update ({}: HttpContextContract) {
-  }
-
-  public async destroy ({}: HttpContextContract) {
+  public async destroy ({ params }: HttpContextContract) {
+    const project = await Project.findByOrFail('id', params.id)
+    await project.delete()
   }
 }
